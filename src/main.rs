c27a135e6163b1edbe17e1fb35a08a757eadf4f4
Yoshiki YAMADA <nyamadandan@gmail.com>
2022-11-27 03:14:01 +0900

    dekita

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
-use std::{collections::HashMap, path, vec};
+use std::vec;
 
 #[derive(Clone, PartialEq)]
 struct Node {
     name: &'static str,
-    weight: i32,
-
+    from: Option<&'static str>,
+    weight: Option<i32>,
     start: bool,
     goal: bool,
-    fix: bool,
+    done: bool,
 }
 
 #[derive(Clone, PartialEq)]
 struct Link {
-    a: Node,
-    b: Node,
-    weight: i32,
-}
-
-#[derive(Clone, PartialEq)]
-struct Path {
-    nodes: Vec<Node>,
+    a_name: &'static str,
+    b_name: &'static str,
     weight: i32,
 }
 
@@ -27,8 +21,9 @@ impl Node {
     pub fn new(name: &'static str) -> Self {
         Node {
             name,
-            weight: 0,
-            fix: false,
+            from: None,
+            weight: None,
+            done: false,
             start: false,
             goal: false,
         }
@@ -36,27 +31,28 @@ impl Node {
 }
 
 impl Link {
-    pub fn new(a: Node, b: Node, weight: i32) -> Self {
-        Link { a, b, weight }
+    pub fn new(a_name: &'static str, b_name: &'static str, weight: i32) -> Self {
+        Link {
+            a_name,
+            b_name,
+            weight,
+        }
     }
 
-    pub fn opposite_node(&self, x: &Node) -> Option<&Node> {
-        let x = x.clone();
-
-        if self.a == x {
-            return Some(&self.b);
+    pub fn opposite_node_name(&self, name: &'static str) -> Option<&'static str> {
+        if self.a_name == name {
+            return Some(&self.b_name);
         }
 
-        if self.b == x {
-            return Some(&self.a);
+        if self.b_name == name {
+            return Some(&self.a_name);
         }
 
         return None;
     }
 
-    pub fn contain_node(&self, x: &Node) -> bool {
-        let x = x.clone();
-        x == self.a || x == self.b
+    pub fn contain_node_name(&self, name: &'static str) -> bool {
+        name == self.a_name || name == self.b_name
     }
 }
 
@@ -67,67 +63,92 @@ fn create_nodes() -> Vec<Node> {
         .collect()
 }
 
-fn mark_node_as_fix<'a>(mut nodes: Vec<Node>, name: &str) -> Vec<Node> {
-    let mut new_nodes = Vec::<Node>::new();
-    let mut ok = false;
-    while !nodes.is_empty() {
-        let mut node = nodes.remove(0);
-        if node.name == name {
-            node.fix = true;
+fn mark_node_as_fix(mut nodes: Vec<Node>, name: &str) -> Vec<Node> {
+    let index = nodes.iter().position(|x| x.name == name).unwrap();
+    let mut node = nodes.get(index).unwrap().clone();
+    node.done = true;
+    nodes.push(node);
+    nodes.swap_remove(index);
+    nodes
+}
+
+fn mark_node_as_start(mut nodes: Vec<Node>, name: &str) -> Vec<Node> {
+    let index = nodes.iter().position(|x| x.name == name).unwrap();
+    let mut node = nodes.get(index).unwrap().clone();
+    node.start = true;
+    nodes.push(node);
+    nodes.swap_remove(index);
+    nodes
+}
+
+fn mark_node_as_goal(mut nodes: Vec<Node>, name: &str) -> Vec<Node> {
+    let index = nodes.iter().position(|x| x.name == name).unwrap();
+    let mut node = nodes.get(index).unwrap().clone();
+    node.goal = true;
+    nodes.push(node);
+    nodes.swap_remove(index);
+    nodes
+}
+
+fn swap_node(mut nodes: Vec<Node>, index: usize, weight: i32, from: &'static str) -> Vec<Node> {
+    let mut node = nodes.get(index).unwrap().clone();
+    node.weight = Some(weight);
+    node.from = Some(from);
+    nodes.push(node);
+    nodes.swap_remove(index);
+    nodes
+}
+
+fn update_nodes(links: &Vec<Link>, mut nodes: Vec<Node>) -> Vec<Node> {
+    let from = if nodes.iter().find(|x| x.start).unwrap().done {
+        nodes
+            .iter()
+            .filter(|x| !x.done && x.weight.is_some())
+            .min_by_key(|x| x.weight)
+    } else {
+        nodes.iter().find(|x| x.start)
+    }
+    .unwrap();
+    let from_name = from.name;
+    let from_weight = from.weight;
+    let connected: Vec<_> = links
+        .iter()
+        .filter(|&x| x.contain_node_name(from_name))
+        .collect();
+    nodes = mark_node_as_fix(nodes, from_name);
+    for link in connected.into_iter() {
+        let to_name = link.opposite_node_name(from_name).unwrap();
+        let to_index = nodes.iter().position(|x| x.name == to_name).unwrap();
+        let to = nodes.get(to_index).unwrap();
+        let to_fix = to.done;
+        if to_fix {
+            continue;
         }
-        new_nodes.push(node);
-    }
-    if !ok {
-        panic!("Could not mark as fix!");
-    }
-    new_nodes
-}
 
-fn mark_node_as_start<'a>(mut nodes: Vec<Node>, name: &str) -> Vec<Node> {
-    let mut new_nodes = Vec::<Node>::new();
-    let mut ok = false;
-    while !nodes.is_empty() {
-        let mut node = nodes.remove(0);
-        if node.name == name {
-            node.start = true;
-            node.fix = true;
-            ok = true;
+        if let Some(from_weight) = from_weight {
+            let new_weight = from_weight + link.weight;
+            if let Some(to_weight) = to.weight {
+                if new_weight < to_weight {
+                    nodes = swap_node(nodes, to_index, new_weight, from_name);
+                }
+            } else {
+                nodes = swap_node(nodes, to_index, new_weight, from_name);
+            }
+        } else {
+            let new_weight = link.weight;
+            if let Some(to_weight) = to.weight {
+                if new_weight < to_weight {
+                    nodes = swap_node(nodes, to_index, new_weight, from_name);
+                }
+            } else {
+                nodes = swap_node(nodes, to_index, new_weight, from_name);
+            }
         }
-        new_nodes.push(node);
     }
-    if !ok {
-        panic!("Could not mark as start!");
-    }
-    new_nodes
+    return nodes;
 }
 
-fn mark_node_as_goal<'a>(mut nodes: Vec<Node>, name: &str) -> Vec<Node> {
-    let mut new_nodes = Vec::<Node>::new();
-    let mut ok = false;
-    while !nodes.is_empty() {
-        let mut node = nodes.remove(0);
-        if node.name == name {
-            node.goal = true;
-            node.fix = true;
-            ok = true;
-        }
-        new_nodes.push(node);
-    }
-    if !ok {
-        panic!("Could not mark as goal!");
-    }
-    new_nodes
-}
-
-fn find_node_by_name<'a>(nodes: &'a Vec<Node>, name: &str) -> Option<&'a Node> {
-    nodes.iter().find(|&x| x.name == name)
-}
-
-fn find_links_by_nodes<'a>(links: &'a Vec<Link>, node: &'a Node) -> Vec<&'a Link> {
-    links.iter().filter(|&x| x.contain_node(node)).collect()
-}
-
-fn create_links(nodes: &Vec<Node>) -> Vec<Link> {
+fn create_links() -> Vec<Link> {
     vec![
         ("a", "b", 5),
         ("a", "c", 4),
@@ -140,25 +161,34 @@ fn create_links(nodes: &Vec<Node>) -> Vec<Link> {
         ("e", "f", 4),
     ]
     .into_iter()
-    .map(|(a, b, weight)| {
-        Link::new(
-            find_node_by_name(nodes, a).unwrap().clone(),
-            find_node_by_name(nodes, b).unwrap().clone(),
-            weight,
-        )
-    })
+    .map(|(a_name, b_name, weight)| Link::new(a_name, b_name, weight))
     .collect()
 }
 
 fn main() {
     let start = "a";
-    let goal = "f";
-    let nodes = mark_node_as_goal(mark_node_as_start(create_nodes(), start), goal);
-    let links = create_links(&nodes);
-}
+    let goal = "e";
 
-#[test]
-fn creates() {
-    let nodes: Vec<Node> = create_nodes();
-    create_links(&nodes);
+    let mut nodes = mark_node_as_goal(mark_node_as_start(create_nodes(), start), goal);
+    let links = create_links();
+    while !nodes.iter().find(|x| x.name == goal).unwrap().done {
+        nodes = update_nodes(&links, nodes);
+    }
+
+    let mut node = nodes.iter().find(|x| x.name == goal).unwrap();
+    let mut results = vec![format!("{}({})", node.name, node.weight.unwrap())];
+    while node.from.is_some() {
+        node = nodes.iter().find(|x| x.name == node.from.unwrap()).unwrap();
+        results.push(format!(
+            "{}({})",
+            node.name,
+            if node.weight.is_some() {
+                node.weight.unwrap()
+            } else {
+                0
+            }
+        ));
+    }
+    results.reverse();
+    println!("Result: {}", results.join(" → "));
 }
