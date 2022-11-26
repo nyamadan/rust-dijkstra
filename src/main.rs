2b227a9680ed1a75d6ec446f16b37f7fbc5d8689
Yoshiki YAMADA <nyamadandan@gmail.com>
2022-11-26 23:45:25 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use std::{collections::HashMap, vec};
+use std::{collections::HashMap, path, vec};
 
 #[derive(Clone, PartialEq)]
 struct Node {
     name: &'static str,
+    weight: i32,
+
+    start: bool,
+    goal: bool,
+    fix: bool,
 }
 
 #[derive(Clone, PartialEq)]
@@ -20,52 +25,106 @@ struct Path {
 
 impl Node {
     pub fn new(name: &'static str) -> Self {
-        Node { name }
+        Node {
+            name,
+            weight: 0,
+            fix: false,
+            start: false,
+            goal: false,
+        }
     }
 }
 
 impl Link {
     pub fn new(a: Node, b: Node, weight: i32) -> Self {
-        if a.name < b.name {
-            Link { a, b, weight }
-        } else {
-            Link { a: b, b: a, weight }
+        Link { a, b, weight }
+    }
+
+    pub fn opposite_node(&self, x: &Node) -> Option<&Node> {
+        let x = x.clone();
+
+        if self.a == x {
+            return Some(&self.b);
         }
+
+        if self.b == x {
+            return Some(&self.a);
+        }
+
+        return None;
+    }
+
+    pub fn contain_node(&self, x: &Node) -> bool {
+        let x = x.clone();
+        x == self.a || x == self.b
     }
 }
 
-impl Path {
-    pub fn new(nodes: Vec<Node>, weight: i32) -> Self {
-        Path { nodes, weight }
-    }
-}
-
-fn create_nodes() -> HashMap<&'static str, Node> {
+fn create_nodes() -> Vec<Node> {
     vec!["a", "b", "c", "d", "e", "f"]
-        .into_iter()
-        .map(|x| (x, Node::new(x)))
+        .iter()
+        .map(|&x| Node::new(x))
         .collect()
 }
 
-fn find_node_by_name<'a>(nodes: &'a Vec<Node>, name: &str) -> &'a Node {
-    nodes.iter().find(|x| x.name == name).unwrap()
+fn mark_node_as_fix<'a>(mut nodes: Vec<Node>, name: &str) -> Vec<Node> {
+    let mut new_nodes = Vec::<Node>::new();
+    let mut ok = false;
+    while !nodes.is_empty() {
+        let mut node = nodes.remove(0);
+        if node.name == name {
+            node.fix = true;
+        }
+        new_nodes.push(node);
+    }
+    if !ok {
+        panic!("Could not mark as fix!");
+    }
+    new_nodes
 }
 
-fn find_links_by_node<'a>(links: &'a Vec<Link>, node: &Node) -> Vec<&'a Link> {
-    let node = node.clone();
-    links
-        .iter()
-        .filter(|x| x.a == node || x.b == node)
-        .collect()
+fn mark_node_as_start<'a>(mut nodes: Vec<Node>, name: &str) -> Vec<Node> {
+    let mut new_nodes = Vec::<Node>::new();
+    let mut ok = false;
+    while !nodes.is_empty() {
+        let mut node = nodes.remove(0);
+        if node.name == name {
+            node.start = true;
+            node.fix = true;
+            ok = true;
+        }
+        new_nodes.push(node);
+    }
+    if !ok {
+        panic!("Could not mark as start!");
+    }
+    new_nodes
 }
 
-fn find_link_from_nodes<'a>(links: &'a Vec<Link>, a: &Node, b: &Node) -> &'a Link {
-    let a = a.clone();
-    let b = b.clone();
-    links
-        .iter()
-        .find(|x| (x.a == a && x.b == b) || (x.a == b && x.b == a))
-        .unwrap()
+fn mark_node_as_goal<'a>(mut nodes: Vec<Node>, name: &str) -> Vec<Node> {
+    let mut new_nodes = Vec::<Node>::new();
+    let mut ok = false;
+    while !nodes.is_empty() {
+        let mut node = nodes.remove(0);
+        if node.name == name {
+            node.goal = true;
+            node.fix = true;
+            ok = true;
+        }
+        new_nodes.push(node);
+    }
+    if !ok {
+        panic!("Could not mark as goal!");
+    }
+    new_nodes
+}
+
+fn find_node_by_name<'a>(nodes: &'a Vec<Node>, name: &str) -> Option<&'a Node> {
+    nodes.iter().find(|&x| x.name == name)
+}
+
+fn find_links_by_nodes<'a>(links: &'a Vec<Link>, node: &'a Node) -> Vec<&'a Link> {
+    links.iter().filter(|&x| x.contain_node(node)).collect()
 }
 
 fn create_links(nodes: &Vec<Node>) -> Vec<Link> {
@@ -83,8 +142,8 @@ fn create_links(nodes: &Vec<Node>) -> Vec<Link> {
     .into_iter()
     .map(|(a, b, weight)| {
         Link::new(
-            find_node_by_name(nodes, a).clone(),
-            find_node_by_name(nodes, b).clone(),
+            find_node_by_name(nodes, a).unwrap().clone(),
+            find_node_by_name(nodes, b).unwrap().clone(),
             weight,
         )
     })
@@ -92,22 +151,14 @@ fn create_links(nodes: &Vec<Node>) -> Vec<Link> {
 }
 
 fn main() {
-    let nodes: Vec<Node> = create_nodes().values().map(|x| x.clone()).collect();
-    let start = find_node_by_name(&nodes, "a");
-    let goal = find_node_by_name(&nodes, "e");
+    let start = "a";
+    let goal = "f";
+    let nodes = mark_node_as_goal(mark_node_as_start(create_nodes(), start), goal);
     let links = create_links(&nodes);
 }
 
 #[test]
-fn creates_and_finds() {
-    let nodes: Vec<Node> = create_nodes().values().map(|x| x.clone()).collect();
-    let b = find_node_by_name(&nodes, "b");
-    assert_eq!("b", b.name);
-
-    let links = create_links(&nodes);
-    let b_links = find_links_by_node(&links, b);
-    assert_eq!(3, b_links.len());
-    for iter in b_links.iter() {
-        assert!(iter.a.name == "b" || iter.b.name == "b")
-    }
+fn creates() {
+    let nodes: Vec<Node> = create_nodes();
+    create_links(&nodes);
 }
